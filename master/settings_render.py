693f50506cbf9eac325d091d4bbba029848ceb39
pycrafted <abdoulayelah@esp.sn>
2025-10-27 14:14:17 +0000

    Remove django-allauth and Google OAuth dependencies2

--- master/settings_render.py
+++ master/settings_render.py
@@ -238,7 +238,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'allauth.account.middleware.AccountMiddleware',  # Middleware Allauth REQUIS
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
